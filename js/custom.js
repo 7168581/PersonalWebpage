b0403f2f47011a4e9144d69aa06e310f859fdf44
7168581 <409038973@qq.com>
2014-10-08 01:38:06 +0100

    addition
    - add blueimp-gallery carousel script
    - add css for 'dropdown menu' when scolling

--- js/custom.js
+++ js/custom.js
@@ -42,15 +42,90 @@ $(document).ready(function() {
 			$('.nav-custom li').css({
 				'padding': '5px 0px 5px 0px'
 			});
+			$('.dropdown').css({
+				'margin-top': '5px'
+			});
 		}
 		else
 		{
 			$('.nav-custom li').css({
 				'padding': '10px 0px 10px 0px'
 			});
+			$('.dropdown').css({
+				'margin-top': '10px'
+			});
 		}
 	});
 	
-/****************************Contact List*****************************/
-	
+/****************************Project Carousel*****************************/
+	/*===========homepage==========*/
+	blueimp.Gallery(
+		//document.getElementById('links').getElementsByTagName('a'),
+		[
+			{
+				href: 'img/rsz_zeplermountbatten.jpg',
+				title: 'School of Electronics and Computer Science (Mountbatten and Zepler buildings)',
+				class: 'partial'
+			},
+			{
+				href: 'img/rsz_avenuecampus.jpg',
+				title: 'Avenue Campus'
+			},
+			{
+				href: 'img/rsz_TurnerSimsConcertHall.jpg',
+				title: 'Turner Sims Concert Hall in Highfield Campus'
+			},
+			{
+				href: 'img/rsz_HartleyLibraryRear.jpg',
+				title: 'Hartley Library in Highfield Campus'
+			},
+		],
+		{
+			container: '#homepage-carousel',
+			carousel: true
+		}
+	);
+	/*===========recommender-system==========*/
+	blueimp.Gallery(
+		[
+			{
+				href: 'img/twitter-backgrounds.jpg',
+				title: 'twitter'
+			},
+			{
+				href: 'img/background.jpg',
+				title: 'background'
+			},
+			{
+				href: 'img/rainbow-wallpaper.jpg',
+				title: 'rainbow'
+			},
+		],
+		{
+			container: '#recommender-system-carousel',
+			carousel: true
+		}
+	);
+		
+	/*===========cloud-gaming==========*/
+	blueimp.Gallery(
+		[
+			{
+				href: 'img/rainbow-wallpaper.jpg',
+				title: 'rainbow'
+			},
+			{
+				href: 'img/twitter-backgrounds.jpg',
+				title: 'twitter'
+			},
+			{
+				href: 'img/background.jpg',
+				title: 'background'
+			},
+		],
+		{
+			container: '#cloud-gaming-carousel',
+			carousel: true
+		}
+	);
 });
